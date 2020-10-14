0cf5357e972284bb3f15019312b864a0e3a883b5
aaaaaaaaaaa <aaaaa>
2020-10-14 14:06:07 +0800

    Initial Commit

---
+++ protectt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  protectt
+//
+//  Created by User09 on 2020/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ protectt/protecttApp.swift
@@ -0,0 +1,17 @@
+//
+//  protecttApp.swift
+//  protectt
+//
+//  Created by User09 on 2020/10/14.
+//
+
+import SwiftUI
+
+@main
+struct protecttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
